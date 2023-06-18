24e17816e7293179cbfde85bdb2eb07f3c5a3fef
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-06-18 10:19:03 +0530

    Implement dao layer

---
+++ back-end/src/main/java/lk/ijse/dep10/pos/dao/custom/OrderDetailDAO.java
@@ -0,0 +1,14 @@
+package lk.ijse.dep10.pos.dao.custom;
+
+import lk.ijse.dep10.pos.dao.CrudDAO;
+import lk.ijse.dep10.pos.entity.OrderDetail;
+import lk.ijse.dep10.pos.entity.OrderDetailPK;
+
+import java.util.List;
+
+public interface OrderDetailDAO extends CrudDAO<OrderDetail, OrderDetailPK> {
+
+    boolean existsOrderDetailByItemCode(String itemCode) throws Exception;
+
+    List<OrderDetail> getOrderByOrderId(String orderId) throws Exception;
+}
