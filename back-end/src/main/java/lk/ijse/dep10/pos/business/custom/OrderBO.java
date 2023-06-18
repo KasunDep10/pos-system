d1bb02581e4f7237fb40c9fe600bfd0cbdc36879
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-06-18 12:02:48 +0530

    Implement business layer

---
+++ back-end/src/main/java/lk/ijse/dep10/pos/business/custom/OrderBO.java
@@ -0,0 +1,19 @@
+package lk.ijse.dep10.pos.business.custom;
+
+import lk.ijse.dep10.pos.business.SuperBO;
+import lk.ijse.dep10.pos.dto.OrderDTO;
+import lk.ijse.dep10.pos.dto.OrderDTO2;
+import lk.ijse.dep10.pos.dto.OrderDetailDTO;
+
+import java.util.List;
+
+public interface OrderBO extends SuperBO {
+
+    Integer placeOrder(OrderDTO orderDTO) throws Exception;
+
+    List<OrderDTO2> searchOrders(String query) throws Exception;
+
+    List<OrderDetailDTO> searchOrderByOrderId(String id) throws Exception;
+
+    Long getIncome(String query) throws Exception;
+}
